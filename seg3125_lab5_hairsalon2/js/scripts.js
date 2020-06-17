8f3b42d09c4e3f09ebcc4695ae5975252b4e6a59
Alexandra Sklokin <alexandrasklokin@gmail.com>
2020-06-16 20:38:53 -0400

    Lab 5 done commit

--- seg3125_lab5_hairsalon2/js/scripts.js
+++ seg3125_lab5_hairsalon2/js/scripts.js
@@ -53,4 +53,65 @@
     navbarCollapse();
     // Collapse the navbar when page is scrolled
     $(window).scroll(navbarCollapse);
+
+    //Selection dropdown changes textfield value
+    $(function(){
+        $(".dropdown-menu-employee").on('click', 'li', function(){
+            $(".dropdown-toggle-employee:first-child").text($(this).text());
+            $(".dropdown-toggle-employee:first-child").val($(this).val());
+        });
+    });
+    
+    $(function(){
+        $(".dropdown-menu-service").on('click', 'li', function(){
+            $(".dropdown-toggle-service:first-child").text($(this).text());
+            $(".dropdown-toggle-service:first-child").val($(this).val());
+        });
+    });
+
+    var unavailableDates = ["06/29/2020","07/07/2020","07/10/2020"]
+
+    const picker = document.getElementById('date');
+    const emp = document.getElementById('specialist');
+
+    picker.addEventListener('input', function(e){
+        var day = new Date(this.value).getUTCDay();
+        if([6,0].includes(day)){
+            e.preventDefault();
+            alert('Weekends are not available. Please choose a weekday.');
+        }
+    });
+
+     picker.addEventListener('input', function(e){
+
+        var employee = emp.value;
+        
+        if(!employee){
+            alert('Select an employee before preceeding to choose a date.');
+        }
+        else {
+            var day = new Date(this.value).getUTCDay();
+
+            if(emp.value=="1" && [4,5].includes(day)){
+                alert('Kay is not available on this day.');
+            }
+            else if(emp.value=="2" && [1,5].includes(day)){
+                alert('Larry is not available on this day.');
+            }
+            else if(emp.value=="3" && [1,2].includes(day)){
+                alert('Diana is not available on this day.');
+            }
+            else if(emp.value=="4" && [1,3,5].includes(day)){
+                alert('Daniel is not available on this day.');
+            }
+            else if(emp.value=="5" && [2,3,4].includes(day)){
+                alert('Mark is not available on this day.');
+            }
+            else if(emp.value=="6" && [1,2,4,5].includes(day)){
+                alert('Larissa is not available on this day.');
+            }
+
+        }
+    });
+
 })(jQuery); // End of use strict
