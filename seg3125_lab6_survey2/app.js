d052faebe6e96da9dcc9eea0075d95a0588e4390
Alexandra Sklokin <alexandrasklokin@gmail.com>
2020-06-23 13:00:22 -0400

    Lab 6 Completed commit

--- seg3125_lab6_survey2/app.js
+++ seg3125_lab6_survey2/app.js
@@ -4,9 +4,9 @@ var app = express();
 
 app.set('view engine','ejs');
 
-app.use(express.static(./public));
+app.use(express.static("./public"));
 
-surveryController(app);
+surveyController(app);
 
 app.listen(3000);
 console.log('listening port 3000');
